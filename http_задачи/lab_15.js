d02972dea8f9d7666a2a137dbe1fbda28ccf8315
Iskander07 <A.iskander07@gmail.com>
2025-09-15 16:04:27 +0600

    добавил 15 лабораторку

---
+++ http_задачи/lab_15.js
@@ -0,0 +1,225 @@
+/* Первое задание
+*
+* "data":[{"id":1,"email":"george.bluth@reqres.in","first_name":"George","last_name":"Bluth",
+    "avatar":"https://reqres.in/img/faces/1-image.jpg"},{"id":2,"email":"janet.weaver@reqres.in",
+    "first_name":"Janet","last_name":"Weaver","avatar":"https://reqres.in/img/faces/2-image.jpg"},
+    {"id":3,"email":"emma.wong@reqres.in","first_name":"Emma","last_name":"Wong",
+        "avatar":"https://reqres.in/img/faces/3-image.jpg"},{"id":4,"email":"eve.holt@reqres.in",
+        "first_name":"Eve","last_name":"Holt","avatar":"https://reqres.in/img/faces/4-image.jpg"},
+    {"id":5,"email":"charles.morris@reqres.in","first_name":"Charles","last_name":"Morris",
+        "avatar":"https://reqres.in/img/faces/5-image.jpg"},{"id":6,"email":"tracey.ramos@reqres.in",
+        "first_name":"Tracey","last_name":"Ramos","avatar":"https://reqres.in/img/faces/6-image.jpg"}]
+*
+* */
+
+/* Второе задание
+*
+* "temperature_2m": [
+      12.2,
+      12.1,
+      12.0,
+      12.4,
+      12.6,
+      13.1,
+      13.2,
+      13.9,
+      15.0,
+      16.6,
+      18.6,
+      20.0,
+      20.4,
+      20.3,
+      20.8,
+      21.1,
+      21.1,
+      20.4,
+      19.4,
+      18.1,
+      17.1,
+      16.3,
+      15.8,
+      16.0,
+      15.0,
+      14.8,
+      14.4,
+      14.0,
+      13.6,
+      13.4,
+      13.7,
+      14.6,
+      15.4,
+      16.2,
+      16.6,
+      17.5,
+      17.9,
+      18.4,
+      18.8,
+      18.1,
+      17.2,
+      15.7,
+      15.0,
+      14.5,
+      14.1,
+      13.9,
+      13.7,
+      13.5,
+      13.3,
+      13.2,
+      13.0,
+      13.1,
+      13.0,
+      12.6,
+      12.3,
+      13.3,
+      15.0,
+      16.5,
+      17.4,
+      18.1,
+      18.6,
+      19.0,
+      19.0,
+      18.4,
+      17.8,
+      17.0,
+      16.4,
+      15.9,
+      15.5,
+      15.4,
+      15.5,
+      15.6,
+      15.8,
+      16.0,
+      16.0,
+      15.9,
+      15.7,
+      15.6,
+      15.8,
+      16.4,
+      17.2,
+      18.0,
+      18.8,
+      19.7,
+      20.4,
+      20.6,
+      20.5,
+      20.4,
+      20.2,
+      19.9,
+      19.6,
+      19.3,
+      19.0,
+      18.8,
+      18.8,
+      18.8,
+      18.8,
+      18.7,
+      18.6,
+      18.5,
+      18.3,
+      18.1,
+      18.1,
+      18.6,
+      19.3,
+      20.2,
+      21.2,
+      22.3,
+      23.2,
+      23.6,
+      23.8,
+      23.6,
+      23.0,
+      22.0,
+      21.0,
+      19.9,
+      18.9,
+      18.0,
+      17.3,
+      16.9,
+      16.6,
+      16.5,
+      16.5,
+      16.5,
+      16.2,
+      16.0,
+      16.5,
+      18.2,
+      20.8,
+      23.1,
+      25.0,
+      26.7,
+      28.0,
+      28.9,
+      29.3,
+      29.0,
+      27.7,
+      25.8,
+      24.0,
+      22.6,
+      21.5,
+      20.5,
+      19.9,
+      19.5,
+      19.2,
+      18.7,
+      18.2,
+      17.9,
+      17.4,
+      17.1,
+      17.5,
+      19.1,
+      21.4,
+      23.5,
+      25.2,
+      26.6,
+      27.7,
+      28.4,
+      28.7,
+      28.5,
+      27.4,
+      25.8,
+      24.3,
+      23.2,
+      22.3,
+      21.6,
+      21.2,
+      21.0
+    ]
+*
+* */
+
+/*
+* Третье задание
+*
+* "form": {
+    "username": "neo, password=trinity"
+  },
+*
+*/
+
+/*
+*
+* Четвертое задание
+*
+*  Тело ответа:
+* {
+  "title": "Matrix",
+  "body": "There is no spoon",
+  "userId": 1,
+  "id": 101
+}
+*
+* */
+
+/* Пятое задание
+*
+* "message": "https://images.dog.ceo/breeds/mastiff-tibetan/n02108551_2326.jpg"
+*
+*/
+
+/* Шестое задание
+*
+* "files": {
+    "file1": "tralalelo tralala"
+  }
+  *
+* */
