7c0341061b355861a7b26370436fa6f261d6aadb
Iskander07 <A.iskander07@gmail.com>
2025-09-15 14:41:44 +0600

    добавил http задачи

---
+++ http_задачи/lab_14.js
@@ -0,0 +1,43 @@
+/* Первое задание
+
+* метод: GET
+* статус-код: 200
+* authority: httpbin.org
+* scheme: https
+* path: /get?course=http&student=neo
+* accept-Language: ru,en,q=0.9
+* user-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 YaBrowser/25.8.0.0 Safari/537.36
+*
+*/
+
+/* Второе задание
+*
+* "args": {
+    "lang": "js",
+    "level": "beginner"
+  },
+*
+* */
+
+/* Третье задание
+*
+* В теле ответа:
+* "args": {
+    "password": "trinity",
+    "username": "neo"
+  },
+* */
+
+/* Четвертое задание
+*
+*   "json": {
+    "task": "lab",
+    "status": "done"
+  },
+* */
+
+/* Пятое задание
+*
+* "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 YaBrowser/25.8.0.0 Safari/537.36",
+* В строке выше написано, что используется яндекс браузер
+* */
