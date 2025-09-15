131d03c689fe482a40db843813b39c53679dea43
Iskander07 <A.iskander07@gmail.com>
2025-09-16 00:12:04 +0600

    завтра закончу

---
+++ http_задачи/lab_16.js
@@ -0,0 +1,28 @@
+/*  Первое задание
+*
+* {
+    "cookies": {
+        "user": "guest_user"
+    }
+}
+*
+* */
+
+// Второе задание
+
+// async function second() {
+//
+//     const URL = "https://httpbin.org/cookies/set?user=guest_user"
+//     await axios.
+//     let result = await axios.get(URL)
+//
+//         .then((res) => {
+//             console.log(res.data.cookies);
+//
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//         })
+// }
+//
+// second()
