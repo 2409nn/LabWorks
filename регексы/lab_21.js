1a0f6bdd04289f9ab3974a51c8209cc7cfedfbb7
Iskanderious <A.iskander07@gmail.com>
2025-09-22 21:23:38 +0500

    доделал

--- регексы/lab_21.js
+++ регексы/lab_21.js
@@ -28,14 +28,39 @@ const fourth = () => {
     const str = `Найти в строке "I like red, green and blue. Green is my favorite color" все
     упоминания цветов red, green, blue.`;
 
-    console.log(str.match(/\b(blue|red|green)\b/gi))
+    console.log(str.match(/\b(blue|red|green)\b/gi));
 }
 
 // fourth();
 
 const fifth = () => {
     const str = "Ivan Petrov";
-    console.log(str.replace(/\b(\w+)\s(\w+)\b/, '$2 $1'))
+    console.log(str.replace(/\b(\w+)\s(\w+)\b/, '$2 $1'));
 }
 
-fifth();
+// fifth();
+
+const sixth = () => {
+    const str = "2025-09-17";
+    console.log(str.replace(/(\d{4})-(\d{2})-(\d{2})/, "$3-$2-$1"));
+}
+
+// sixth();
+
+const seventh = () => {
+    let str = '87011234567';
+    str = str.replace(/^\d/, '+7 ');
+    str = str.replace(/\b(\d{3})(\d{3})(\d{2})(\d{2})/, "($1) $2-$3-$4");
+    console.log(str);
+}
+
+// seventh()
+
+const eighth = (password) => {
+    console.log(/^[A-Za-z][A-Za-z0-9]{5,}$/.test(password));
+}
+
+// eighth('F21334');
+// eighth('qwerty123');
+// eighth('ю9323123');
+// eighth('c418');
